78f7f5141fb0ad9ee062229ec2c960d64119c5d0
cs61b-bjw <jessiehua88@berkeley.edu>
2018-03-29 01:07:52 -0700

    param

--- mnist.py
+++ mnist.py
@@ -2,17 +2,23 @@ import argparse, random, sys
 import tensorflow as tf
 import numpy as np
 
-
 # Download dataset
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets(FLATS.data_dir, one_hot=TRUE)
 
+# Hyperparmeters
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch', type = int. default = 100)
+parser.add_argument('--layers', type = int. default = 3)
+args = parser.parse_args()
+print("Our arguments:\n{}".format(args))
 
 def make_ff_layer(size, x):
-    x = tf.nn.relu(tf.keras.layers.Dense(size)(x))
-    x = tf.nn.relu(tf.keras.layers.Dense(size)(x))
+    for i in range(1, args.layers):
+        x = tf.nn.relu(tf.keras.layers.Dense(size)(x))
     x = tf.keras.layers.Dense(10)(x)
     return x
+
 
 
 
