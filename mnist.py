e52e095c72929132c3788ea09f83500efae9f410
cs61b-bjw <jessiehua88@berkeley.edu>
2018-03-29 00:52:52 -0700

    ff

--- mnist.py
+++ mnist.py
@@ -1,5 +1,19 @@
 import argparse, random, sys
-from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
+
+
+# Download dataset
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets(FLATS.data_dir, one_hot=TRUE)
+
+
+def make_ff_layer(size, x):
+    x = tf.nn.relu(tf.keras.layers.Dense(size)(x))
+    x = tf.nn.relu(tf.keras.layers.Dense(size)(x))
+    x = tf.keras.layers.Dense(10)(x)
+    return x
+
+
+
 
