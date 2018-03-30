982eb98347263b636c6adf2d3eb6f0b5a6944643
DanielTakeshi <takeshidanny@gmail.com>
2018-03-30 16:53:23 -0700

    Got it running (on my computer with a GPU)

--- mnist.py
+++ mnist.py
@@ -1,5 +1,5 @@
 import argparse, random, sys
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 import tensorflow as tf
 import numpy as np
@@ -16,21 +16,66 @@ parser.add_argument('--size', type = int, default=784)
 args = parser.parse_args()
 print("Our arguments:\n{}".format(args))
 
-# Constructing layer
-def make_ff_layer(size, x):
-    for i in range(1, args.layers):
-        x = tf.nn.relu(tf.keras.layers.Dense(size)(x))
-    x = tf.keras.layers.Dense(10)(x)
-    return x
 
-x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32, [None, 10])
-x, y = mnist.train.next_batch(args.batch)
-y_logits = make_ff_layer(args.size, x)
-y_softmax = tf.nn.softmax(y_logits)
+def get_tf_session(gpumem):
+    """ Returning a session. Set options here if desired. """
+    tf.reset_default_graph()
+    tf_config = tf.ConfigProto(inter_op_parallelism_threads=1,
+                               intra_op_parallelism_threads=1)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpumem)
+    session = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+    def get_available_gpus():
+        from tensorflow.python.client import device_lib
+        local_device_protos = device_lib.list_local_devices()
+        return [x.physical_device_desc for x in local_device_protos if x.device_type == 'GPU']
+    print("AVAILABLE GPUS: ", get_available_gpus())
+    return session
 
-plt.plot(y, y_softmax)
+session = get_tf_session(0.5)
+# Define computational graph
+x_tf = tf.placeholder(tf.float32, [None, 784])
+y_tf = tf.placeholder(tf.float32, [None, 10])
 
 
+x_1 = tf.nn.relu(tf.keras.layers.Dense(200)(x_tf))
+x_2 = tf.nn.relu(tf.keras.layers.Dense(200)(x_1))
+y_logits = tf.keras.layers.Dense(10)(x_2)
+
+#y_logits = make_ff_layer(x_tf)
+cost = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_tf, logits=y_logits)
+)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
+
+correct_prediction = tf.equal(tf.argmax(y_logits,1), tf.argmax(y_tf,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Initialize session
+session.run(tf.global_variables_initializer())
+
+## Debugging
+#variables = tf.trainable_variables()
+#print("\nHere are the variables in our network:")
+#params = 0
+#for item in variables:
+#    print(item)
+#    params += np.prod(item.get_shape().as_list())
+#    print("Total net parameters: {}\n".format(params))
+#sys.exit()
 
 
+# Feed in data to train network.
+num_minibatches = int(mnist.train.images.shape[0] / args.batch)
+
+for epoch in range(100):
+    for minibatch in range(num_minibatches):
+        x, y = mnist.train.next_batch(args.batch)
+        feed = {x_tf: x, y_tf: y}
+        _,ce_cost = session.run([train_step, cost], feed_dict=feed)
+
+    feed_t = {
+        x_tf: mnist.test.images,
+        y_tf: mnist.test.labels,
+    }
+    acc = session.run(accuracy, feed_dict=feed_t)
+    print("epoch {}, accuracy {}".format(epoch, acc))
